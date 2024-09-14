7b7414c2c1990cfd93defefc27e15a78219f6c6b
yangwang <wyang107@ucr.edu>
2024-09-14 18:27:47 +0000

    1

--- different_step.py
+++ different_step.py
@@ -2,15 +2,17 @@ import yaml
 import sys
 import os
 sys.path.append(os.path.join(sys.path[0], '../'))
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import pickle
-from datasets import load_dataset
 import torch 
 import numpy as np
-from utils.trace_utils import TraceDict2
-from utils.model_utils import try_hook_ground
+from utils.model_utils import load_model
 
+class step_info:
+    def __init__(self,hidden_states,key,values,ground_attentions) -> None:
+        self.hidden_states=hidden_states
+        self.key=key
+        self.values=values
+        self.ground_attentions=ground_attentions
 
 def exploit_data(model,tokenizer,model_config):
     with torch.inference_mode():
@@ -18,11 +20,22 @@ def exploit_data(model,tokenizer,model_config):
         prompt = 'Beats Music is owned by'
         inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
         output_and_cache = model(**inputs, output_hidden_states=True, output_attentions=True)
+        hidden_states = torch.cat(output_and_cache.hidden_states, dim=0).detach().cpu().numpy()
+        key = torch.cat([k[0] for k in output_and_cache.past_key_values], dim=0).detach().cpu().numpy()
+        values = torch.cat([k[1] for k in output_and_cache.past_key_values], dim=0).detach().cpu().numpy()
         ground_attentions = torch.cat(output_and_cache.attentions, dim=0).detach().cpu().numpy()
+        data = step_info(hidden_states, key,values,ground_attentions)
         # k,q,v,hidden states collection
         prompt = 'The concept of artificial intelligence (AI) has captured the imagination of scientists, engineers, and enthusiasts alike for decades. AI refers to the simulation of human intelligence in machines, enabling them to perform tasks that typically require human cognition, such as learning, problem-solving, and decision-making. Over the years, AI has evolved from simple rule-based systems to complex neural networks capable of remarkable feats, including natural language processing, image recognition, and autonomous driving. One of the most significant advancements in AI has been the development of deep learning, a subfield of machine learning that uses artificial neural networks with many layers to learn from vast amounts of data. Deep learning algorithms have demonstrated unprecedented accuracy in various domains, revolutionizing industries such as healthcare, finance, and transportation. However, despite its tremendous potential, AI also raises ethical concerns and challenges. Issues related to bias in algorithms, data privacy, job displacement, and the existential risk posed by superintelligent'
-        info = try_hook_ground(model, tokenizer, model_config, prompt, device)
-        return {'info':info, 'attn_sink':ground_attentions}
+        # info = try_hook_ground(model, tokenizer, model_config, prompt, device)
+        inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+        output_and_cache = model(**inputs, output_hidden_states=True, output_attentions=True)
+        hidden_states = torch.cat(output_and_cache.hidden_states, dim=0).detach().cpu().numpy()
+        key = torch.cat([k[0] for k in output_and_cache.past_key_values], dim=0).detach().cpu().numpy()
+        values = torch.cat([k[1] for k in output_and_cache.past_key_values], dim=0).detach().cpu().numpy()
+        ground_attentions = torch.cat(output_and_cache.attentions, dim=0).detach().cpu().numpy()
+        data2 = step_info(hidden_states, key,values,ground_attentions)
+        return {'long': data, 'short':data2}
 
 
 def save_data(data, save_path):
@@ -32,6 +45,20 @@ def save_data(data, save_path):
 device = 'cuda:0'
 save_path = os.path.join(sys.path[0], './results/')
 if not os.path.exists(save_path): os.makedirs(save_path)
+
+## instruct
+models = ['Qwen/Qwen2-7B', 'meta-llama/Llama-2-7b-hf', 'meta-llama/Llama-2-7b-chat-hf',
+          'meta-llama/Llama-2-13b-hf', 'meta-llama/Llama-2-13b-chat-hf',
+          'Qwen/Qwen2-7B' , 'Qwen/Qwen2-7B-Instruct',
+          'Qwen/Qwen-14B', 'Qwen/Qwen-14B-Chat']
+save_path_tmp = save_path
+for model_name in models:
+    p = model_name.split('/')[1]
+    if not os.path.exists(os.path.join(save_path_tmp, f'{p}.dat')):
+        cache_dir = os.path.join(f"./model_cache/{p}")
+        model, tokenizer, model_config = load_model(model_name, device, cache_dir=cache_dir)
+        result = exploit_data(model, tokenizer, model_config)
+        save_data(result, os.path.join(save_path_tmp, f'{p}.dat'))
 
 ## pythia
 save_path_tmp = os.path.join(save_path, 'pythia')
@@ -43,24 +70,8 @@ for model_size in model_size_list:
     for revision in revisions:
         if not os.path.exists(os.path.join(save_path_tmp, f'{revision}.dat')):
             cache_dir = os.path.join(f"./model_cache/{model_name}/{revision}")
-            model = AutoModelForCausalLM.from_pretrained(model_name,revision=f"step{revision}",cache_dir=cache_dir).to(device)
-            model_config = model.config
-            tokenizer = AutoTokenizer.from_pretrained(model_name,revision=f"step{revision}",cache_dir=cache_dir)
+            model, tokenizer, model_config = load_model(model_name, device, cache_dir=cache_dir)
             result = exploit_data(model, tokenizer, model.config)
-            save_data(result, os.path.join(save_path_tmp, revision))
+            save_data(result, os.path.join(save_path_tmp, f'{revision}.dat'))
 
-## instruct
-models = ['meta-llama/Llama-2-7b-hf', 'meta-llama/Llama-2-7b-chat-hf',
-          'meta-llama/Llama-2-13b-hf', 'meta-llama/Llama-2-13b-chat-hf',
-          'Qwen/Qwen2-7B' , 'Qwen/Qwen2-7B-Instruct',
-          'Qwen/Qwen-14B', 'Qwen/Qwen-14B-Chat']
-save_path_tmp = save_path
-for model_name in models:
-    p = model_name.split('/')[1]
-    if not os.path.exists(os.path.join(save_path_tmp, f'{p}.dat')):
-        cache_dir = os.path.join(f"./model_cache/{model_name}")
-        model = AutoModelForCausalLM.from_pretrained(model_name,revision=f"step{revision}",cache_dir=cache_dir).to(device)
-        model_config = model.config
-        tokenizer = AutoTokenizer.from_pretrained(model_name,revision=f"step{revision}",cache_dir=cache_dir)
-        result = exploit_data(model, tokenizer, model.config)
-        save_data(result, os.path.join(save_path_tmp, revision))
+
--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,6 +1,214 @@
 from utils.trace_utils import TraceDict2
 import torch
 import numpy as np
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+def load_model(model_name: str, device='cuda', cache_dir='', low_cpu_mem_usage=False, show_params=True):
+
+    model = AutoModelForCausalLM.from_pretrained(model_name, low_cpu_mem_usage=low_cpu_mem_usage, 
+                                                 token='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',
+                                                 cache_dir=cache_dir).to(device)
+    device, dtype = model.device, model.dtype
+
+    model_modules = dict(model.named_modules())
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name,token='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')
+    tokenizer.pad_token = tokenizer.eos_token
+  
+    MODEL_CONFIG = model.config
+
+    if model_name.find('qiwen') !=-1:
+        MODEL_CONFIG = {    
+                        'name':'qiwen',
+                        "n_heads": model.config.num_attention_heads,
+                        "n_layers": model.config.num_hidden_layers,
+                        "resid_dim": model.config.hidden_size,
+                        "name_or_path": model.config.name_or_path,
+                        'result_dir':'qiwen',
+                        "blocks": [f'model.layers.{layer}' for layer in
+                                            range(model.config.num_hidden_layers)],
+                         'wte': ['lm_head']
+                        }
+
+    if model_name.find('pythia') != -1:
+        MODEL_CONFIG = {"n_heads": model.config.num_attention_heads,
+                        "n_layers": model.config.num_hidden_layers,
+                        "resid_dim": model.config.hidden_size,
+                        "name_or_path": model.config.name_or_path,
+                        'result_dir':'phi_2',
+                        "k_names": [f'model.layers.{layer}.self_attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,          
+                        "v_names": [f'model.layers.{layer}.self_attn.v_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "q_names": [f'model.layers.{layer}.self_attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "k_q_names": [f'model.layers.{layer}.self_attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] + 
+                                    [f'model.layers.{layer}.self_attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "out_proj": [f'model.layers.{layer}.self_attn.dense' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_in": [f'model.layers.{layer}.mlp.fc1' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_out": [f'model.layers.{layer}.mlp.fc2' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "blocks": [f'model.layers.{layer}' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        }
+
+    if model_name.find('phi-2') != -1:
+        MODEL_CONFIG = {"n_heads": model.config.num_attention_heads,
+                        "n_layers": model.config.num_hidden_layers,
+                        "resid_dim": model.config.hidden_size,
+                        "name_or_path": model.config.name_or_path,
+                        'result_dir':'phi_2',
+                        "k_names": [f'model.layers.{layer}.self_attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,          
+                        "v_names": [f'model.layers.{layer}.self_attn.v_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "q_names": [f'model.layers.{layer}.self_attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "k_q_names": [f'model.layers.{layer}.self_attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] + 
+                                    [f'model.layers.{layer}.self_attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "out_proj": [f'model.layers.{layer}.self_attn.dense' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_in": [f'model.layers.{layer}.mlp.fc1' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_out": [f'model.layers.{layer}.mlp.fc2' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "blocks": [f'model.layers.{layer}' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        }
+
+
+    if model_name.find('gpt-neo') != -1:
+        MODEL_CONFIG = {"n_heads": model.config.num_attention_heads,
+                        "n_layers": model.config.num_hidden_layers,
+                        "resid_dim": model.config.hidden_size,
+                        "name_or_path": model.config.name_or_path,
+                        'result_dir':'gpt_neo',
+                        "attn_hook_names": [f'transformer.h.{layer}.attn.attention.attn_dropout' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "k_names": [f'transformer.h.{layer}.attn.attention.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,          
+                        "v_names": [f'transformer.h.{layer}.attn.attention.v_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "q_names": [f'transformer.h.{layer}.attn.attention.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "k_q_names": [f'transformer.h.{layer}.attn.attention.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] + 
+                                    [f'transformer.h.{layer}.attn.attention.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "out_proj": [f'transformer.h.{layer}.attn.attention.out_proj' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_in": [f'transformer.h.{layer}.mlp.c_fc' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_out": [f'transformer.h.{layer}.mlp.c_proj' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "blocks": [f'transformer.h.{layer}' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        }
+
+    if model_name.find('llama') != -1:
+        MODEL_CONFIG = {"n_heads": model.config.num_attention_heads,
+                        "n_layers": model.config.num_hidden_layers,
+                        "resid_dim": model.config.hidden_size,
+                        "name_or_path": model.config.name_or_path,
+                        'result_dir':'llama2_7b',
+                        "k_names": [f'model.layers.{layer}.self_attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,          
+                        "v_names": [f'model.layers.{layer}.self_attn.v_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "q_names": [f'model.layers.{layer}.self_attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "k_q_names": [f'model.layers.{layer}.self_attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] + 
+                                    [f'model.layers.{layer}.self_attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "out_proj": [f'model.layers.{layer}.self_attn.o_proj' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_in": [f'model.layers.{layer}.mlp.up_proj' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_out": [f'model.layers.{layer}.mlp.down_proj' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "blocks": [f'model.layers.{layer}' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        }
+        if model_name.find('7b') != -1: MODEL_CONFIG['result_dir'] = 'llama2_7b'
+        if model_name.find('8b') != -1: MODEL_CONFIG['result_dir'] = 'llama3_8b'
+        if model_name.find('13b-chat') != -1: MODEL_CONFIG['result_dir'] = 'llama2_13b_chat'
+    
+    if model_name.find('gpt-j') !=-1:
+        MODEL_CONFIG = {    
+                        'name':'gptj',
+                        "n_heads": model.config.num_attention_heads,
+                        "n_layers": model.config.num_hidden_layers,
+                        "resid_dim": model.config.hidden_size,
+                        "name_or_path": model.config.name_or_path,
+                        'result_dir':'gpt_j',
+                        "attn_hook_names": [f'transformer.h.{layer}.attn.attn_dropout' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "k_names": [f'transformer.h.{layer}.attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,          
+                        "v_names": [f'transformer.h.{layer}.attn.v_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "q_names": [f'transformer.h.{layer}.attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "k_q_names": [f'transformer.h.{layer}.attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] + 
+                                    [f'transformer.h.{layer}.attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "out_proj": [f'transformer.h.{layer}.attn.out_proj' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_out": [f'transformer.h.{layer}.mlp.fc_out' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_in": [f'transformer.h.{layer}.mlp.fc_in' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "blocks": [f'transformer.h.{layer}' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        }
+        
+    if model_name.find('opt') !=-1:
+        MODEL_CONFIG = {"n_heads": model.config.num_attention_heads,
+                        "n_layers": model.config.num_hidden_layers,
+                        "resid_dim": model.config.hidden_size,
+                        "name_or_path": model.config.name_or_path,
+                        'result_dir':'llama2_7b',
+                        "k_names": [f'model.decoder.layers.{layer}.self_attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,          
+                        "v_names": [f'model.decoder.layers.{layer}.self_attn.v_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "q_names": [f'model.decoder.layers.{layer}.self_attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "k_q_names": [f'model.decoder.layers.{layer}.self_attn.k_proj' for layer in
+                                            range(model.config.num_hidden_layers)] + 
+                                    [f'model.decoder.layers.{layer}.self_attn.q_proj' for layer in
+                                            range(model.config.num_hidden_layers)] ,
+                        "out_proj": [f'model.decoder.layers.{layer}.self_attn.out_proj' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_in": [f'model.decoder.layers.{layer}.fc1' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "fc_out": [f'model.decoder.layers.{layer}.fc2' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        "blocks": [f'model.decoder.layers.{layer}' for layer in
+                                            range(model.config.num_hidden_layers)],
+                        }
+        if model_name.find('opt-6.7b') != -1: MODEL_CONFIG['result_dir'] = 'opt_6b'
+        if model_name.find('opt-1.3b') != -1: MODEL_CONFIG['result_dir'] = 'opt_1b'
+        if model_name.find('opt-2.7b') != -1: MODEL_CONFIG['result_dir'] = 'opt_2b'
+        
+
+    if show_params:
+        for name, param in model.named_parameters():
+            if param.requires_grad:
+                # print(torch.isnan(param.grad).any())
+                # print('name:{} param grad:{} param requires_grad:{},params:{}'.format(name, param.grad, param.requires_grad,param))
+                print('name:{} param requires_grad:{}, detype:{}, device:{}'.format(name, param.requires_grad, param.dtype, param.device))
+
+    return model, tokenizer, MODEL_CONFIG
+
 
 class Info:
     def __init__(self, attn_input ,attn_output,mlp_input,mlp_output, hidden_state, ground_attentions, past_key, past_values, past_qs) -> None:
