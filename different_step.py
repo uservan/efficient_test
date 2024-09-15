1486e2889c021f29ae802227c03414a561459e2c
xuanyu <1650096073@qq.com>
2024-09-14 17:05:01 -0700

    1

--- different_step.py
+++ different_step.py
@@ -26,8 +26,8 @@ def exploit_data(model,tokenizer,model_config):
         ground_attentions = torch.cat(output_and_cache.attentions, dim=0).detach().cpu().numpy()
         data = step_info(hidden_states, key,values,ground_attentions)
         # k,q,v,hidden states collection
-        # prompt = 'The concept of artificial intelligence (AI) has captured the imagination of scientists, engineers, and enthusiasts alike for decades. AI refers to the simulation of human intelligence in machines, enabling them to perform tasks that typically require human cognition, such as learning, problem-solving, and decision-making. Over the years, AI has evolved from simple rule-based systems to complex neural networks capable of remarkable feats, including natural language processing, image recognition, and autonomous driving. One of the most significant advancements in AI has been the development of deep learning, a subfield of machine learning that uses artificial neural networks with many layers to learn from vast amounts of data. Deep learning algorithms have demonstrated unprecedented accuracy in various domains, revolutionizing industries such as healthcare, finance, and transportation. However, despite its tremendous potential, AI also raises ethical concerns and challenges. Issues related to bias in algorithms, data privacy, job displacement, and the existential risk posed by superintelligent'
-        prompt = 'The concept of artificial intelligence (AI) has captured the imagination of scientists, engineers, and enthusiasts alike for decades. AI refers to the simulation of human intelligence in machines, enabling them to perform'
+        prompt = 'The concept of artificial intelligence (AI) has captured the imagination of scientists, engineers, and enthusiasts alike for decades. AI refers to the simulation of human intelligence in machines, enabling them to perform tasks that typically require human cognition, such as learning, problem-solving, and decision-making. Over the years, AI has evolved from simple rule-based systems to complex neural networks capable of remarkable feats, including natural language processing, image recognition, and autonomous driving. One of the most significant advancements in AI has been the development of deep learning, a subfield of machine learning that uses artificial neural networks with many layers to learn from vast amounts of data. Deep learning algorithms have demonstrated unprecedented accuracy in various domains, revolutionizing industries such as healthcare, finance, and transportation. However, despite its tremendous potential, AI also raises ethical concerns and challenges. Issues related to bias in algorithms, data privacy, job displacement, and the existential risk posed by superintelligent'
+        # prompt = 'The concept of artificial intelligence (AI) has captured the imagination of scientists, engineers, and enthusiasts alike for decades. AI refers to the simulation of human intelligence in machines, enabling them to perform'
         inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
         output_and_cache = model(**inputs, output_hidden_states=True, output_attentions=True)
         hidden_states = torch.cat(output_and_cache.hidden_states, dim=0).detach().cpu().numpy()
@@ -46,20 +46,6 @@ device = 'cuda:0'
 save_path = os.path.join(sys.path[0], './results/')
 if not os.path.exists(save_path): os.makedirs(save_path)
 
-## instruct
-models = ['Qwen/Qwen2-1.5B', 'meta-llama/Llama-2-7b-hf', 'meta-llama/Llama-2-7b-chat-hf',
-          'meta-llama/Llama-2-13b-hf', 'meta-llama/Llama-2-13b-chat-hf',
-          'Qwen/Qwen2-7B' , 'Qwen/Qwen2-7B-Instruct',
-          'Qwen/Qwen2-1.5B', 'Qwen/Qwen2-1.5B-Instruct']
-save_path_tmp = save_path
-for model_name in models:
-    p = model_name.split('/')[1]
-    if not os.path.exists(os.path.join(save_path_tmp, f'{p}.dat')):
-        cache_dir = os.path.join(f"./model_cache/{p}")
-        model, tokenizer, model_config = load_model(model_name, device, cache_dir=cache_dir)
-        result = exploit_data(model, tokenizer, model_config)
-        save_data(result, os.path.join(save_path_tmp, f'{p}.dat'))
-
 ## pythia
 save_path_tmp = os.path.join(save_path, 'pythia')
 if not os.path.exists(save_path_tmp): os.makedirs(save_path_tmp)
@@ -76,5 +62,19 @@ for model_size in model_size_list:
             result = exploit_data(model, tokenizer, model.config)
             path = os.path.join(save_path_tmp,model_size,f'{revision}.dat')
             save_data(result, path)
+
+## instruct
+models = ['Qwen/Qwen2-1.5B', 'meta-llama/Llama-2-7b-hf', 'meta-llama/Llama-2-7b-chat-hf',
+          'meta-llama/Llama-2-13b-hf', 'meta-llama/Llama-2-13b-chat-hf',
+          'Qwen/Qwen2-7B' , 'Qwen/Qwen2-7B-Instruct',
+          'Qwen/Qwen2-1.5B', 'Qwen/Qwen2-1.5B-Instruct']
+save_path_tmp = save_path
+for model_name in models:
+    p = model_name.split('/')[1]
+    if not os.path.exists(os.path.join(save_path_tmp, f'{p}.dat')):
+        cache_dir = os.path.join(f"./model_cache/{p}")
+        model, tokenizer, model_config = load_model(model_name, device, cache_dir=cache_dir)
+        result = exploit_data(model, tokenizer, model_config)
+        save_data(result, os.path.join(save_path_tmp, f'{p}.dat'))
 
 
